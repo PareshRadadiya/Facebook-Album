650d82203406b6a43637577cd23658345fff96fa
Paresh <pareshpravin@gmail.com>
2014-03-08 19:24:24 +0530

    intialization of project on git

---
+++ index.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html>
+	<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Facebook Album</title>
+    <link rel="stylesheet" type="text/css" href="css/normalize.css">
+        <link rel="stylesheet" type="text/css" href="css/foundation.min.css">
+      <link type="text/css" rel="stylesheet" href="css/fbalbum.css">
+    <script src="js/vendor/modernizr.js"></script>
+     <script src="js/vendor/jquery.js"></script>
+     <script src="js/foundation.min.js"></script>
+    
+    <script>
+  $(document).foundation();
+</script>
+  
+
+</head>
+  <body>
+   
+    <div class="off-canvas-wrap">
+      <div class="inner-wrap">
+       
+
+         <title>Facebook Album</title>
+         
+ 
+
+
+
+
+<nav class="top-bar" data-topbar="">
+  <ul class="title-area">
+    <li class="name">
+      <h1><a href="#">FBAlbum</a></h1>
+    </li>
+  </ul>
+
+</nav>
+
+        
+
+
+
+<section id="homepage-hero">
+
+  <div class="row">
+    <div class="medium-7 large-6 columns">
+      <h1>FB Album</h1><br>
+      <h3>Download your Facebook albums <br class="hide-for-small">and move it to picasa.</h3>
+      <br>
+    </div>
+  </div>
+
+
+  <div class="row">
+    <div class="large-4 medium-6 columns">
+      <a href="http://foundation.zurb.com/develop/download.html" class="large button hide-for-small fbstyle">Connect with facebook</a>
+      <a href="http://foundation.zurb.com/develop/download.html" class="small button show-for-small fbstyle">Connect with facebook</a>
+    </div>
+  </div>
+
+  
+  <div class="floatingyeti">
+    <img src="img/Facebook_like_thumb.png">
+  </div>
+</section>
+        <a class="exit-off-canvas"></a>
+      </div>      
+    </div>
+   
+  
+
+</body></html>
---
+++ php-sdk/Fbcredentials.php
@@ -0,0 +1,14 @@
+<?php
+  // Remember to copy files from the SDK's src/ directory to a
+  // directory in your application on the server, such as php-sdk/
+  require_once('php-sdk/facebook.php');
+
+  $config = array(
+    'appId' => '209721652515800',
+    'secret' => '6d4dc86a9b34d4a9ef23299b47ef24b1',
+    'allowSignedRequest' => false // optional but should be set to false for non-canvas apps
+  );
+
+  $facebook = new Facebook($config);
+  $user_id = $facebook->getUser();
+?>
